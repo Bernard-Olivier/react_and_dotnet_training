22134f1aa7128b9887ca611eb31401b412420b58
Bernard Olivier <88403974+Bazz94@users.noreply.github.com>
2023-12-27 10:55:30 +0200

    Lesson 27

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +35,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,26 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+// import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [activities, setActivites] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
+      setActivites(response.data);
+    });
+  }, []);
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Reactivities</h1>
+      <ul>
+        {activities.map((activity: any) => (
+          <li key={activity.id}>
+            {activity.title}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
