b6e278544818556f6b8a3c99e4e23810e91c4a7d
Bernard Olivier <88403974+Bazz94@users.noreply.github.com>
2024-01-02 19:59:15 +0200

    Lesson 114 (S10)

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
 import "./app/layout/style.css";
+import "react-toastify/dist/ReactToastify.min.css";
 import "react-calendar/dist/Calendar.css";
 import { StoreContext, store } from "./app/stores/store";
 import { RouterProvider } from "react-router-dom";
@@ -10,7 +11,7 @@ import { router } from "./app/router/routes";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <StoreContext.Provider value={store}>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </StoreContext.Provider>
   </React.StrictMode>
 );
